650df4dd5e13452c1dbc71462f2390729b66a578
FlyingIdeal <963161475@qq.com>
2017-03-17 17:55:42 +0800

    remove vue2.0 project

--- src/components/Foot.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<div class="footer">
-		<div class="wid1210">
-			<span>Copyright © 2014-2017, www.flyideal.com, All Rights Reserved</span>
-		</div>
-	</div>
-</template>
-<script></script>
